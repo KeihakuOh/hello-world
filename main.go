c2220f870ffd9ed948b271d47c5c7f3500ee1b57
KeihakuOh <w1123527817@akane.waseda.jp>
2024-04-14 16:18:12 +0900

    2-8finished

--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println("Hello, world.")
+
+	var whatToSay string
+	var i int
+
+	whatToSay = "Goodbye, cruel world"
+	fmt.Println(whatToSay)
+
+	i = 7
+	fmt.Println("i is set to", i)
+	fmt.Println("i is set to")
+
+	whatWasSaid, theOtherThingThatWasSaid := saySomething()
+
+	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid)
+}
+
+func saySomething() (string, string) {
+	return "something", "else"
 }
