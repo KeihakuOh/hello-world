493d3d2ec7928145c3ca460eeab7e8a8f543cc6a
KeihakuOh <w1123527817@akane.waseda.jp>
2024-04-14 16:47:45 +0900

    section2-10

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	user := User{
+		FirstName:   "Trevor",
+		LastName:    "Sawler",
+		PhoneNumber: "1 555 555 1212",
+	}
+
+	log.Println(user.FirstName, user.LastName, "Birthdate:", user.BirthDate)
 }
