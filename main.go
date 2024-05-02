6d53aed00ae622456ee1752dcea71435ac135e87
KeihakuOh <w1123527817@akane.waseda.jp>
2024-05-02 23:56:11 +0900

    section2-25

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	"log"
-
-	"github.com/KeihakuOh/Go-learning/helpers"
+	"fmt"
+	"html/template"
+	"net/http"
 )
 
-func main() {
-	var myVar helpers.SomeType
-	myVar.TypeName = "Some name"
+const portNumber = ":8080"
 
-	log.Println(myVar.TypeName)
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+	}
+}
+
+// main is the main function
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
