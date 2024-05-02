dca53b2c40e06ccf569f7a2dec64f82f9a412dee
KeihakuOh <w1123527817@akane.waseda.jp>
2024-05-02 19:55:15 +0900

    section2-23

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"log"
-
-	"github.com/KeihakuOh/Go-learning/helpers"
+	"fmt"
+	"net/http"
 )
 
-func main() {
-	var myVar helpers.SomeType
-	myVar.TypeName = "Some name"
+const portNumber = ":8080"
 
-	log.Println(myVar.TypeName)
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d"), sum)
+}
+
+// addValues adds two ints x and y, and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main function
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
